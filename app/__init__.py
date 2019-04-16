499d06653b50f0b1630949d0585f1a13e742e899
Roman Dzieciol <roman.dzieciol@gmail.com>
2019-04-16 20:46:13 +0100

    Add localisation

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_babel import Babel
 
 import logging
 from logging.handlers import SMTPHandler
@@ -13,6 +14,7 @@ import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import lazy_gettext as _l
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -22,10 +24,12 @@ migrate = Migrate(app, db)
 
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -56,3 +60,10 @@ if not app.debug:
     app.logger.info('Microblog startup')
 
 from app import routes, models, errors
+
+from flask import request
+
+@babel.localeselector
+def get_locale():
+    return 'es'
+    #return request.accept_languages.best_match(app.config['LANGUAGES'])
